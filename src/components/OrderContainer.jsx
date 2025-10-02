a573f5bb51023d43ab2b064aed17445dfa89c14e
Masum Islam Badsha <nkfhhdndjdh@gmail.com>
2025-10-02 10:25:20 +0600

    update change

--- src/components/OrderContainer.jsx
+++ src/components/OrderContainer.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import States from './States';
+import OrderCard from './Cards/OrderCard';
 
 const OrderContainer = ({orderPromise}) => {
   const orders = use(orderPromise)
@@ -8,6 +9,21 @@ const OrderContainer = ({orderPromise}) => {
   return (
     <div>
       <States></States>
+      <section className=' container mx-auto py-10 gird grid-cols-1 md:grid-cols-12 gap-5 '>
+        <div className='md:col-span-7'>
+          <h2 className='font-bold text-4xl '></h2>
+          <div className='space-y-5'>
+            {
+              orders.map(order =>
+                <OrderCard key={order.id} order={order}></OrderCard>
+              )
+            }
+
+          </div>
+        </div>
+        <div className='md:col-span-5'>
+        </div>
+      </section>
     </div>
   );
 };
